e51711b1a07ea934480636570b5473456c897074
Nurullah Akkaya <nurullah@nakkaya.com>
2013-09-26 05:36:05 +0300

    update bash config

--- resources/site/dotfiles/bootstrap.sh
+++ resources/site/dotfiles/bootstrap.sh
@@ -47,7 +47,7 @@ wget https://raw.github.com/technomancy/leiningen/stable/bin/lein
 chmod +x lein
 sudo mv lein /usr/bin/
 
-#sudo apt-get update;sudo apt-get upgrade;sudo apt-get install emacs24 xmonad trayer vlc cmus git feh rxvt-unicode-256color offlineimap gnupg lm-sensors build-essential ubuntu-restricted-extras vilistextum graphviz openjdk-6-jdk keynav tmux sysstat
+#sudo apt-get update;sudo apt-get upgrade;sudo apt-get install emacs24 xmonad trayer vlc cmus git bash-completion feh rxvt-unicode-256color offlineimap gnupg lm-sensors build-essential ubuntu-restricted-extras vilistextum graphviz openjdk-6-jdk keynav tmux sysstat
 
 #wget -qO- http://127.0.0.1:8000/bootstrap.sh | bash
 #wget -qO- https://raw.github.com/nakkaya/nakkaya.com/master/resources/site/dotfiles/bootstrap.sh | bash
