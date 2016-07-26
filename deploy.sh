5f59c39c4c628d1e9e13fa1f0b6d17980f809d86
Nurullah Akkaya <nurullah@nakkaya.com>
2016-07-26 17:10:01 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-s3cmd sync --delete --acl-public html/ s3://nakkaya.com
-s3cmd --mime-type=application/rss+xml --acl-public put html/rss-feed s3://nakkaya.com/rss-feed
+rsync --progress -az html/ base:/var/nakkaya.com/
