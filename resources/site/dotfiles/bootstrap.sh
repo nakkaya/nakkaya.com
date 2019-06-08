8a1477a04b8e518319c3a291a880cb804b0511ff
Nurullah Akkaya <nurullah@nakkaya.com>
2019-06-08 22:21:15 +0300

    remove unused code

--- resources/site/dotfiles/bootstrap.sh
+++ resources/site/dotfiles/bootstrap.sh
@@ -18,16 +18,6 @@ function tangleFile(){
 }
 
 function tangle(){
-    rm -f ~/.xmobarrc
-    rm -rf ~/.xmonad
-    rm -f ~/.keynavrc
-    rm -f ~/.Xresources
-    tangleFile site/dotfiles/ xmonad
-
-    rm -rf ~/.pentadactyl
-    rm -f ~/.pentadactylrc
-    tangleFile posts/ 2014-01-26-pentadactyl-configuration
-
     rm -f ~/.tmux.conf
     tangleFile posts/ 2014-01-05-tmux-configuration
     chmod +x ~/.bin/ta-dev
@@ -36,7 +26,6 @@ function tangle(){
     rm -f ~/.profile
     tangleFile site/dotfiles/ bash
 
-    mkdir -p ~/annex
     mkdir -p ~/source
     tangleFile posts/ 2013-10-23-notes-on-synchronization-and-backup-of-home-using-git-git-annex-and-mr
 }
@@ -69,8 +58,6 @@ case "$1" in
        chmod +x ~/.bin/lein
 
        wget https://raw.githubusercontent.com/nakkaya/nakkaya.com/master/resources/site/dotfiles/desktop-files/arduino.desktop -P ~/.local/share/applications
-       wget https://raw.githubusercontent.com/nakkaya/nakkaya.com/master/resources/site/dotfiles/desktop-files/refbox.desktop -P ~/.local/share/applications
-       wget https://raw.githubusercontent.com/nakkaya/nakkaya.com/master/resources/site/dotfiles/desktop-files/grsim.desktop -P ~/.local/share/applications
        
        gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
        gsettings set com.canonical.Unity.Lenses remote-content-search none
@@ -78,4 +65,3 @@ case "$1" in
 esac
 
 exit $RETVAL
-
